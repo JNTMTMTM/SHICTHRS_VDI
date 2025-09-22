d106af1bace4398c30a27b0051b2b3e86824d659
JNTMTMTM <15251865468@163.com>
2025-09-22 20:51:17 +0800

    V0.0.0 add VDI - namespace

---
+++ src/data/vdi_var.py
@@ -0,0 +1,6 @@
+
+import os
+
+class vdi_var():
+    def __init__(self):
+        self.EXEPATH : str = os.getcwd()
--- src/vdi_main.py
+++ src/vdi_main.py
@@ -13,20 +13,33 @@
 import sys , os
 sys.path.append("..")
 
-from PySide6.QtWidgets import (QApplication , QMainWindow)
+from PySide6.QtWidgets import QApplication , QMainWindow
 from PySide6 import QtGui , QtWidgets
 from ui.vdi_ui import Ui_vdi
+from data.vdi_var import vdi_var
 
 class vdi_gui(Ui_vdi , QMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.show()
+    
+    
 
 
 if __name__ == "__main__":
+    # 激活全局变量
+    var = vdi_var()
+    
+    # 初始化应用程序
     app = QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon(os.path.join(os.getcwd() , 'icon' , 'logo_32x32.ico')))
+
+    # 设置图标
+    app.setWindowIcon(QtGui.QIcon(os.path.join(var.EXEPATH , 'icon' , 'logo_32x32.ico')))
+
+    # 使用fusion主题
     app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
+    
+    # 初始化主窗口
     vdi_gui = vdi_gui()
     sys.exit(app.exec())
